783745e53cee2af05baad3c3cd2272472f1c002b
ye.wang <905317742@qq.com>
2023-02-09 11:35:04 +0800

    run脚本修改

--- run_examples.sh
+++ run_examples.sh
@@ -2,10 +2,10 @@ echo "Launching Euroc Dataset with Monocular-Inertial sensor"
 
 # euroc dataset
 # root=/media/psf/work/data/euroc_data
-# ./Examples/Monocular-Inertial/mono_inertial_euroc ./Vocabulary/ORBvoc.bin ./Examples/Monocular-Inertial/EuRoC.yaml $root/MH_01 ./Examples/Monocular-Inertial/EuRoC_TimeStamps/MH01.txt dataset-MH01_monoi
+# ./Examples/Monocular-Inertial/mono_inertial_euroc ./Vocabulary/ORBvoc.bin ./Examples/Monocular-Inertial/EuRoC.yaml $root/MH_01_easy ./Examples/Monocular-Inertial/EuRoC_TimeStamps/MH01.txt dataset-MH01_monoi
 
 # phone-slamdemo dataset
-root="/media/psf/work/project/20230116 slam数据采集和耗时统计/data"
+root="/media/psf/work/data/slam/mate20/dataset/indoor_walking_loop_staticinit_2"
 dataset_name=dataset-mate20_monoi
-./Examples/Monocular-Inertial/mono_inertial_euroc ./Vocabulary/ORBvoc.bin ./Examples/Monocular-Inertial/mate20-640.yaml "$root" "$root/mav0/cam0/data.csv" $dataset_name
-# ./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.bin ./Examples/Monocular-Inertial/mate20.yaml "$root" "$root/mav0/cam0/data.csv" $dataset_name
+# ./Examples/Monocular-Inertial/mono_inertial_euroc ./Vocabulary/ORBvoc.bin ./Examples/Monocular-Inertial/mate20-640.yaml "$root" "$root/mav0/cam0/timestamp.csv" $dataset_name
+./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.bin ./Examples/Monocular-Inertial/mate20-640.yaml "$root" "$root/mav0/cam0/timestamp.csv" $dataset_name
